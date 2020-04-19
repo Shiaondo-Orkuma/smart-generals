5aa46400592f1e6577860ba93fd55089e0d9d112
Shiaondo Orkuma <shiaondoorkuma@gmail.com>
2020-04-19 13:09:42 +0100

    Updates

---
+++ precache-manifest.4e4f9b6aad1c6c31d06aeba0e142d291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a430bf47b390bc260ff5beb448f9b04",
+    "url": "/smart-generals/index.html"
+  },
+  {
+    "revision": "405025e4ea7b09fde2b6",
+    "url": "/smart-generals/static/css/main.92b7e17d.chunk.css"
+  },
+  {
+    "revision": "20b1912bf242704f9f11",
+    "url": "/smart-generals/static/js/2.0a6b9ac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-generals/static/js/2.0a6b9ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405025e4ea7b09fde2b6",
+    "url": "/smart-generals/static/js/main.2ccfbd26.chunk.js"
+  },
+  {
+    "revision": "83fa59b2fc8ed955168d",
+    "url": "/smart-generals/static/js/runtime-main.f59b45c9.js"
+  }
+]);
